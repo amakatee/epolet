03bc628d3f1af0221d6e48be177e9f06cf733868
Ekaterina Zinina <ekaterinazinina@Ekaterinas-MacBook-Pro.local>
2022-05-02 20:48:14 +0300

    img fix

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,17 +6,19 @@ import {HiPhone } from 'react-icons/hi'
 import {ImLocation2} from 'react-icons/im'
 import {SiMinutemailer} from 'react-icons/si'
 import Form from './Form'
+import { client } from '../lib/client'
 
 
 
 const Footer = () => {
+
   return (
     <>
     <div className='footer-container'>
       <div className='footer-map'>
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8917.63961575856!2d35.488728094063845!3d56.02889124004799!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46ca09664b8741bd%3A0xe4c839d5d6a10304!2sEpolet!5e0!3m2!1sen!2sru!4v1651418085413!5m2!1sen!2sru"
       
-      width="360" height="250"   allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      width="360" height="250"   allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
   
       </div>
       <div className='footer-contact'>
@@ -37,5 +39,6 @@ const Footer = () => {
       </>
   )
 }
+
 
 export default Footer
